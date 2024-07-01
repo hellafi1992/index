f71a348f9814d11a4b5f1d81458151e04d52c9b0
hellafi1992 <121656343+hellafi1992@users.noreply.github.com>
2024-07-01 18:35:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const form = document.getElementById('login-form');
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+const errorMessage = document.getElementById('error-message');
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+    var user1 = 'admin1';
+    var pass1 = '0001';
+
+    var user2 = 'admin2';
+    var pass2 = '0002';
+
+	var user3 = 'admin3';
+	var pass3 = '0003'
+
+	document.getElementById("error-message").style.fontWeight = "900";
+
+	const username = usernameInput.value.trim();
+	const password = passwordInput.value.trim();
+
+	if (username === user1 && password === pass1 || username === user2 && password === pass2 || username === user3 && password === pass3) {
+
+		window.location.href = 'index0.html';
+	} else {
+		errorMessage.textContent = 'كلمة المرور خـطئ !';
+	}
+});
+
+document.getElementById("button");
+document.getElementById("error-message");
+
+function Button(){
+	document.getElementById('error-message').style.display = "none";
+	window.location.href = 'index.html';
+}
+
+function Submit(){
+	document.getElementById('error-message');
+	document.getElementById('button').style.display = "block";
+}
---
+++ scripts.js
@@ -0,0 +1,43 @@
+document.getElementById('error-message').style.background = 'none';
+document.getElementById('error-message').style.display = 'block';
+document.getElementById("error-message").style.animation = "Hide 5s linear";
+
+
+document.getElementById('username').style.color = 'red';
+document.getElementById('username').style.fontWeight = '800';
+document.getElementById('username').style.textAlign = 'center';
+
+document.getElementById('password').style.color = 'red';
+document.getElementById('password').style.fontWeight = '800';
+document.getElementById('password').style.textAlign = 'center';
+
+
+document.getElementById('submit').style.color = 'blue';
+document.getElementById('submit').style.fontWeight = '800';
+document.getElementById('submit').style.textAlign = 'center';
+
+
+document.getElementById('button').style.color = 'blue';
+document.getElementById('button').style.fontWeight = '800';
+document.getElementById('button').style.textAlign = 'center';
+
+
+
+
+document.getElementById('h2').innerHTML = 'Window The Login';
+document.getElementById('h2').style.color = 'blue';
+document.getElementById('h2').style.fontWeight = '800';
+document.getElementById('h2').style.textAlign = 'center';
+
+
+document.getElementById('h1').innerHTML = 'Login';
+document.getElementById('h1').style.color = 'brown';
+document.getElementById('h1').style.fontWeight = '900';
+document.getElementById('h1').style.textAlign = 'center';
+
+
+
+document.getElementById('footer').innerHTML = '&copf; 2024 حمد اللافي';
+document.getElementById('footer').style.color = 'black';
+document.getElementById('footer').style.fontWeight = '900';
+document.getElementById('footer').style.textAlign = 'center';
